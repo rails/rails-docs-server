9fc9c75d54a07564643d381b3efd454ae5cd2a31
localhostdotdev <root@localhost.dev>
2019-03-21 03:20:57 +0100

    Configurable path that defaults to the gitignored ./checkout/ directory
    Prevents polluting home directory on non-CI/prod servers

--- bin/generate_docs.rb
+++ bin/generate_docs.rb
@@ -5,11 +5,23 @@ $:.unshift(File.expand_path('../lib', __dir__))
 require 'lock_file'
 require 'docs_generator'
 require 'git_manager'
+require 'fileutils'
+
+if ARGV.size > 1 || ARGV.first == "-h" || ARGV.first == "--help"
+  puts "USAGE: bin/generate_docs.rb [CHECKOUT_PATH]"
+  exit
+end
+
+CHECKOUT_PATH = ARGV.first || File.join(File.dirname(__FILE__), '../checkout')
+
+unless Dir.exists?(CHECKOUT_PATH)
+  FileUtils.mkdir(CHECKOUT_PATH)
+end
 
 LockFile.acquiring('docs_generation.lock') do
-  git_manager = GitManager.new(Dir.home)
+  git_manager = GitManager.new(CHECKOUT_PATH)
   git_manager.update_master
 
-  generator = DocsGenerator.new(Dir.home, git_manager)
+  generator = DocsGenerator.new(CHECKOUT_PATH, git_manager)
   generator.generate
 end
