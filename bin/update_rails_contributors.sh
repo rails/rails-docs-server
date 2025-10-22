3901798a33cb35bc73fcff698d72fdfccb6edf7c
Rafael Mendonça França <rafael@rubyonrails.org>
2025-10-22 00:25:21 +0000

    Fix bin/update_rails_contributors.sh rvm command

--- bin/update_rails_contributors.sh
+++ bin/update_rails_contributors.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-nice --adjustment=19 rvm in $HOME/rails-contributors/current bin/rails runner -e production Repo.sync
+nice --adjustment=19 rvm in $HOME/rails-contributors/current do bin/rails runner -e production Repo.sync
