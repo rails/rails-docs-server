4bf45947cd8e87fdac1bb1b55b4fa9c2c6d5d1fc
Rafael Mendonça França <rafael@rubyonrails.org>
2025-11-01 16:49:57 +0000

    Handle RVM scripts that expect unset variables

--- bin/update_rails_contributors.sh
+++ bin/update_rails_contributors.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# RVM bootstrap expects some variables to be unset
+set +u
 set -ex
 
 nice --adjustment=19 rvm in $HOME/rails-contributors/current do bin/rails runner -e production Repo.sync
